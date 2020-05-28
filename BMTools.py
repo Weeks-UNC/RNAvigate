eb5dff2cdda2cdd5e69c1a5afe0a72eaddff4c20
Patrick Irving <psirving@gmail.com>
2020-05-28 13:40:35 -0400

    Added tools to handle EnsembleMap reactivities.txt files

---
+++ BMTools.py
@@ -0,0 +1,27 @@
+import plottingTools as pt
+import pandas as pd
+
+
+class BM():
+    """A class for handling reactivities.txt files"""
+    def __init__(self, reactivities):
+        with open(reactivities) as inf:
+            header1 = inf.readline().strip().split()
+            header2 = inf.readline().strip().split()
+        self.components = int(header1[0])
+        self.p = header2[1:]
+        colnames = ["Nucleotide", "Sequence"]
+        for i in range(self.components):
+            colnames.append("nReact"+i)
+            colnames.append("Raw"+i)
+        self.reactivities = pd.read_csv(reactivities, sep='\t', header=2,
+                                        names=colnames)
+
+    def plotBMmodel(self, axis):
+        """Plot all profiles as skyline. Legend includes population values."""
+        for i in range(self.components):
+            pt.plotSkyline(axis, self.reactivities,
+                           label="{}: {}".format(i, self.p[i]),
+                           column="nReact{}".format(i))
+        pt.addSeqBar(axis, self.reactivities)
+        axis.legend(title="Component: Population")
