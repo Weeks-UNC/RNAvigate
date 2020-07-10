0ab4303141d04f7205fc228ce6a5b31a1fd14f23
Patrick <psirving@email.unc.edu>
2020-07-10 12:41:56 -0400

    updates

--- plottingTools.py
+++ plottingTools.py
@@ -56,7 +56,14 @@ def readHistograms(logfile):
                 untmuts = []
                 for x in flist[i+32:i+53]:
                     untmuts.append(float(x.strip().split('\t')[1]))
-    data = {'Modified_read_length': modlength,
+    data = {'Read_length': ['(0,49)', '(50,99)', '(100,149)', '(150,199)',
+                            '(200,249)', '(250,299)', '(300,349)', '(350,399)',
+                            '(400,449)', '(450,499)', '(500,549)', '(550,599)',
+                            '(600,649)', '(650,699)', '(700,749)', '(750,799)',
+                            '(800,849)', '(850,899)', '(900,949)', '(950,999)',
+                            '>1000'],
+            'Mutation_count': range(21),
+            'Modified_read_length': modlength,
             'Modified_mutations_per_molecule': modmuts,
             'Untreated_read_length': untlength,
             'Untreated_mutations_per_molecule': untmuts}
