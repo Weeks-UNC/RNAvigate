8ae5e8268bd2084fb090c37571327174312d71c6
Patrick Irving <psirving@gmail.com>
2020-07-15 11:54:32 -0400

    combined bitmap functions

--- JNBarcPlot.py
+++ JNBarcPlot.py
@@ -90,13 +90,12 @@ def setArcPlot(ax):
     ax.spines['top'].set_color('none')
 
 
-def plotCorrs2D(axis, pairmap=None, allcorrs=None, bg_corrs=None, ct=None,
-                mask=None):
+def plotCorrs2D(axis, pairmap=0, allcorrs=0, bg_corrs=0, ct=0, mask=0):
     ct_cmap, mask_cmap, pm_cmap = getCmaps()
     if type(allcorrs) is np.ndarray:
-        axis.imshow(allcorrs, cmap='bwr', interpolation='bicubic')
+        axis.imshow(allcorrs, cmap='bwr')
     if type(bg_corrs) is np.ndarray:
-        axis.imshow(bg_corrs, cmap='gray', interpolation='bicubic')
+        axis.imshow(bg_corrs, cmap='gray')
     if type(pairmap) is np.ndarray:
         axis.imshow(pairmap, cmap=pm_cmap)
     if type(ct) is np.ndarray:
@@ -138,92 +137,85 @@ def getCmaps():
     vals[:, 2] = np.concatenate((np.linspace(1, 1, N/2),
                                  np.linspace(1, 0, N/2)), axis=None)
     pm_cmap = mpl.colors.ListedColormap(vals)
-
     return ct_cmap, mask_cmap, pm_cmap
 
 
-def fastaToMask(fastafile):
-    with open(fastafile) as f:
-        f.readline()
-        seq = ""
-        for line in f.readlines():
-            seq += line.strip()
-    size = len(seq)
-    mask = np.zeros((size, size))
-    for i in range(size):
-        for j in range(size):
-            if (i > j) or seq[i].islower() or seq[j].islower():
-                mask[i, j] = 1
-    return mask
+def toBitmap(filepath, ctpath='none', type='none', window=1, limit=200):
+    valid_types = ['fasta', 'ct', 'corr', 'log', 'pairmap']
+    if type not in valid_types:
+        print('Please provide a valid type: ' + ', '.join(valid_types))
+        return
+    elif type == 'fasta':
+        with open(filepath) as f:
+            f.readline()
+            seq = ""
+            for line in f.readlines():
+                seq += line.strip()
+        size = len(seq)
+        bitmap = np.zeros((size, size))
+        for i in range(size):
+            for j in range(size):
+                if (i > j) or seq[i].islower() or seq[j].islower():
+                    bitmap[i, j] = 1
+    elif type == 'ct':
+        size = ctLength(filepath)
+        bitmap = np.ones((size, size))
+        names = ['i', 'j']
+        ct = pd.read_csv(filepath, sep='\s+', names=names,
+                         header=0, usecols=[0, 4])
+        ct = ct[ct.i < ct.j]
+        for x in [-1, 0, 1]:
+            for y in [-1, 0, 1]:
+                bitmap[ct['i']+x, ct['j']+y] = 0
+    elif type == 'corr':
+        size = ctLength(ctpath)
+        bitmap = np.zeros((size, size))
+        corrs = pd.read_csv(filepath, sep='\t', header=1)
+        for i in range(window*window):
+            x = i % window
+            y = int(i/window)
+            bitmap[corrs.i+x, corrs.j+y] += corrs.Statistic*corrs['+/-']
+            bitmap[bitmap > limit] = limit
+            bitmap[bitmap < -limit] = -limit
+            bitmap = bitmap/limit
+            bitmap[0, 0] = 1
+            bitmap[0, 1] = -1
+            bitmap += 1
+            bitmap /= 2
+    elif type == 'log':
+        size = ctLength(ctpath)
+        bitmap = np.ones((size, size))
+        with open(filepath) as f:
+            for line in f.readlines():
+                if line.startswith('Pair '):
+                    pair = line.split()[1].strip('()').split(',')
+                    bitmap[pair[0], pair[1]] = 0
+    elif type == 'pairmap':
+        size = ctLength(ctpath)
+        bitmap = np.zeros((size, size))
+        pairs = pd.read_csv(filepath, sep='\t', header=1)
+        primary = pairs[pairs['Class'] == 1]
+        secondary = pairs[pairs['Class'] == 2]
+        for i in range(9):
+            x = i % 3
+            y = int(i/3)
+            bitmap[secondary['i']+x, secondary['j']+y] = 0.5
+            for i in range(9):
+                x = i % 3
+                y = int(i/3)
+                bitmap[primary['i']+x, primary['j']+y] = 1
+    return bitmap
 
 
-def ctLength(ctfile):
-    with open(ctfile) as f:
+def ctLength(ctpath):
+    with open(ctpath) as f:
         line1 = f.readline()
         length = int(line1.strip().split()[0])
         return length
 
 
-def ignoredToBitmap(logfile, ctfile):
-    size = ctLength(ctfile)
-    bitmap = np.ones((size, size))
-    with open(logfile) as f:
-        for line in f.readlines():
-            if line.startswith('Pair '):
-                pair = line.split()[1].strip('()').split(',')
-                bitmap[pair[0], pair[1]] = 0
-    return bitmap
-
-
-def pairmapSensPPV(pairfile, ctfile):
-    pm = pma.PairMap(pairfile)
-    ct = RNA.CT(ctfile, filterNC=True, filterSingle=True)
-    p,s = pm.ppvsens_duplex(ct, ptype=1, exact=False)
-
-
-def corrsToBitmap(corrfile, ctfile, window=1, limit=100):
-    size = ctLength(ctfile)
-    bitmap = np.zeros((size, size))
-    corrs = pd.read_csv(corrfile, sep='\t', header=1)
-    for i in range(window*window):
-        x = i % window
-        y = int(i/window)
-        bitmap[corrs['i']+x, corrs['j']+y] += corrs['Statistic']*corrs['+/-']
-    bitmap[bitmap > limit] = limit
-    bitmap[bitmap < -limit] = -limit
-    bitmap = bitmap/limit
-    bitmap[0, 0] = 1
-    bitmap[0, 1] = -1
-    bitmap += 1
-    bitmap /= 2
-    return bitmap
-
-
-def ctToBitmap(ctfile):
-    size = ctLength(ctfile)
-    bitmap = np.ones((size, size))
-    names = ['i', 'j']
-    ct = pd.read_csv(ctfile, sep='\s+', names=names,
-                     header=0, usecols=[0, 4])
-    ct = ct[(ct['j'] != 0) & (ct['i'] < ct['j'])]
-    for x in [-1, 0, 1]:
-        for y in [-1, 0, 1]:
-            bitmap[ct['i']+x, ct['j']+y] = 0
-    return bitmap
-
-
-def pairmapToBitmap(pairfile, ctfile):
-    size = ctLength(ctfile)
-    bitmap = np.zeros((size, size))
-    pairs = pd.read_csv(pairfile, sep='\t', header=1)
-    primary = pairs[pairs['Class'] == 1]
-    secondary = pairs[pairs['Class'] == 2]
-    for i in range(9):
-        x = i % 3
-        y = int(i/3)
-        bitmap[secondary['i']+x, secondary['j']+y] = 0.5
-    for i in range(9):
-        x = i % 3
-        y = int(i/3)
-        bitmap[primary['i']+x, primary['j']+y] = 1
-    return bitmap
+def pairmapSensPPV(pairpath, ctpath):
+    pm = pma.PairMap(pairpath)
+    ct = RNA.CT(ctpath, filterNC=True, filterSingle=True)
+    p, s = pm.ppvsens_duplex(ct, ptype=1, exact=False)
+    return p, s
