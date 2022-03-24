43be509313a7d232d459b3d17dfe0879bfccfb4f
Patrick Irving <psirving@gmail.com>
2022-03-23 22:46:46 -0400

    added main __init__.py contents

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .starmapper import *
+import starmapper.styles
+
+__all__ = ["Sample", "array_qc", "array_ap", "array_skyline", "array_ss",
+           "array_heatmap", "array_mol", "array_circle", "array_linreg",
+           "create_code_button"]
