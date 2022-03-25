7e91c0b3f23b4508d8b78bfd88a18e508a46dbdd
Patrick Irving <psirving@gmail.com>
2022-03-24 22:50:21 -0400

    added method docstrings

--- starmapper/starmapper.py
+++ starmapper/starmapper.py
@@ -41,9 +41,60 @@ class Sample():
                  pairs=None,
                  allcorrs=None,
                  pdb=None,
-                 pdb_kwargs={},
+                 pdb_kwargs={'chain': 'A'},
                  probs=None,
                  dance_prefix=None):
+        """Creates a sample object which connects all chemical probing and
+        structural data for a single experiment. Contains convenience methods
+        to plot, filter, compare and retrieve this data.
+
+        Args:
+            sample (str, optional): This string will serve as a label for this
+                sample within plots.
+                Defaults to None.
+            fasta (str, optional): Path to a fasta file which will provide the
+                reference sequence for ShapeJumper Data.
+                Defaults to None.
+            profile (str, optional): Path to a ShapeMapper profile.txt output
+                file.
+                Defaults to None.
+            rnp (str, optional): Path to an RNPMapper .csv file.
+                Defaults to None.
+            ct (str, optional): Path to a .ct, .dbn, or .bracket structure
+                file.
+                Defaults to None.
+            compct (str, optional): Same as ct above, but serves as a
+                comparison structure for plotting.
+                Defaults to None.
+            ss (str, optional): Path to a .xrna, .varna, .nsd, or .cte
+                secondary structure drawing file.
+                Defaults to None.
+            log (str, optional): Path to a ShapeMapper shapemapper_log.txt
+                output file.
+                Defaults to None.
+            rings (str, optional): Path to a RingMapper output file.
+                Defaults to None.
+            deletions (str, optional): Path to a ShapeJumper output file.
+                Defaults to None.
+            pairs (str, optional): Path to a PairMapper pairmap.txt output
+                file.
+                Defaults to None.
+            allcorrs (str, optional): Path to a PairMapper allcorrs.txt output
+                file.
+                Defaults to None.
+            pdb (str, optional): Path to a .pdb atom coordinates file.
+                Defaults to None.
+            pdb_kwargs (dict, optional): Dictionary containing info needed to
+                parse provided .pdb. "chain" is required. "fasta" and "offset"
+                may be required if not provided in header.
+                Defaults to {'chain':'A'}.
+            probs (dict, optional): Path to an RNAStructure .dp pairing
+                probability file.
+                Defaults to None.
+            dance_prefix (str, optional): Path prefix for DanceMapper output
+                files.
+                Defaults to None.
+        """
         self.paths = {"fasta": fasta,
                       "profile": profile,
                       "rnp": rnp,
@@ -104,6 +155,13 @@ class Sample():
             self.init_dance(dance_prefix)
 
     def init_dance(self, prefix):
+        """Initializes a list of Sample objects which each represent a
+        component of the DANCE model, available as Sample.dance.
+
+        Args:
+            prefix (str): Path to DanceMapper output file prefixes. Finds
+                profiles, rings, pairs, and structure predictions if present.
+        """
         reactivityfile = f"{prefix}-reactivities.txt"
         # read in 2 line header
         with open(reactivityfile) as inf:
@@ -148,6 +206,18 @@ class Sample():
 ###############################################################################
 
     def get_data(self, key):
+        """Internal function that returns a data object associated with key.
+        Prevents an error and prints a message if data object not found. It
+        will also search the parent of a DANCE model component for structure
+        information.
+
+        Args:
+            key (str): Descriptor of data type desired. Can be any of the keys
+               of Sample.data or "label"
+
+        Returns:
+            Data object
+        """
         if key == "label":
             return self.sample
         elif key == None:
@@ -162,6 +232,12 @@ class Sample():
                 return None
 
     def get_data_list(self, *keys):
+        """Calls Sample.get_data() on a list of keys, also accepts "ctcompare",
+        which is equivalent to ["ct", "compct"]
+
+        Returns:
+            list of data objects
+        """
         data_list = []
         for key in keys:
             if key == "ctcompare":
@@ -178,6 +254,17 @@ class Sample():
         return data_list
 
     def filter_ij(self, ij, fit_to, **kwargs):
+        """Fits ij Data object to fit_to Data object's sequence. For example,
+        for plotting IJ data containing structure cassettes against a CT
+        without, or plotting sequence variants together using a best pairwise
+        alignment. Kwargs are passed to IJ.filter(). Refer to that method for
+        more detail on filters.
+
+        Args:
+            ij (IJ): IJ object to be filtered and fitted
+            fit_to (Data (any)): Data object containing a sequence for IJ to be
+                fit to for plotting purposes.
+        """
         try:
             metric = kwargs.pop("metric")
             if metric == "Distance":
@@ -191,6 +278,21 @@ class Sample():
 
     def dance_filter(self, filterneg=True, cdfilter=15, sigfilter=23,
                      ssfilter=True):
+        """Applies a standard filter to plot DANCE rings, pairs, and predicted
+        structures together.
+
+        Args:
+            filterneg (bool, optional): Remove negative correlations.
+                Defaults to True.
+            cdfilter (int, optional): Filters rings by contact distance based
+                on predicted structures for *ALL* DANCE components.
+                Defaults to 15.
+            sigfilter (int, optional): Lower bound for MI.
+                Defaults to 23.
+            ssfilter (bool, optional): Filters out rings with at least one leg
+                in a double stranded region based on that DANCE component.
+                Defaults to True.
+        """
         kwargs = {}
         if filterneg:
             kwargs["Sign"] = 0
@@ -220,15 +322,23 @@ class Sample():
 ###############################################################################
 
     def make_qc(self, **kwargs):
+        """Makes a plot of ShapeMapper log data and reactivities.
+
+        Returns:
+            starmapper.plots.QC
+        """
         return array_qc([self], **kwargs)
 
     def make_skyline(self, dance=False):
+
         if dance:
             plot = array_skyline(self.dance)
             plot.ax.legend(title="Comp: Percent")
             plot.ax.set_title(f"{self.sample}: DANCE Reactivities")
+            return plot
         else:
             array_skyline([self])
+            return plot
 
     def make_shapemapper(self, plots=["profile", "rates", "depth"]):
         plot = SM(self.data["profile"].length, plots=plots)
@@ -236,11 +346,37 @@ class Sample():
         return plot
 
     def make_ap(self, ct="ct", comp="compct", ij=None, ij2=None,
-                profile="profile", label="label", **kwargs):
+                profile="profile", label="label", dance=False, **kwargs):
+        if dance:
+            plot = array_ap(self.dance, ct, comp, "pairs", "rings", profile,
+                            label, **kwargs)
+            for i, dance in enumerate(self.dance):
+                ax = plot.get_ax(i)
+                ax.set_title(
+                    f"DANCE component: {i}, Percent: {self.dance_percents[i]}")
         return array_ap([self], ct, comp, ij, ij2, profile, label, **kwargs)
 
     def make_ap_multifilter(self, filters, ct="ct", comp="compct", ij2=None,
                             profile="profile", label="label"):
+        """Makes an array of arc plots of different filtered views of data from
+        Sample.
+
+        Args:
+            filters (dict): Dictionary containing filtering kwargs for each
+                plot in this array. Requires "ij" and "fit_to".
+            ct (str, optional): key for data object to use as ct.
+                Defaults to "ct".
+            comp (str, optional): key for data object to use as comparison ct.
+                Defaults to "compct".
+            ij2 (_type_, optional): key for data object to use as second ij.
+                Defaults to None.
+            profile (str, optional): key for data object to use as profile.
+                Defaults to "profile".
+            label (str, optional): Defaults to "label", same as Sample.sample.
+
+        Returns:
+            starmapper.plots.AP
+        """
         plot = AP(len(filters), self.get_data(ct).length)
         for filter in filters:
             ij = filter.pop("ij")
