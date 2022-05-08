1e12fe04fc9d45fc66e2cc54b9d4a6f6adedbbb6
Patrick Irving <psirving@gmail.com>
2022-05-08 14:32:11 -0400

    align to struct, region contour, aesthetics

--- starmapper/plots/heatmap.py
+++ starmapper/plots/heatmap.py
@@ -6,35 +6,55 @@ from .plots import Plot
 
 
 class Heatmap(Plot):
-    def __init__(self, num_samples):
-        super().__init__(num_samples)
-        self.pass_through = ["levels"]
+    def __init__(self, num_samples, structure):
+        super().__init__(num_samples, sharey=True)
+        self.structure = structure
+        for i in range(num_samples):
+            ax = self.get_ax(i)
+            ax.set_aspect("equal")
+            mn_mx = (0.5, structure.length + 0.5)
+            ax.set(ylim=mn_mx, xlim=mn_mx,
+                   xticks=list(range(50, structure.length+1, 50)))
+        self.pass_through = ["levels", "regions", "interpolation"]
 
-    def plot_data(self, structure, ij, levels=None):
+    def plot_data(self, ij, label, levels=None, regions=None,
+                  interpolation='none'):
         ax = self.get_ax()
-        self.plot_contour_distances(ax, structure, ij, levels)
-        self.plot_heatmap_data(ax, structure, ij)
+        if regions is not None:
+            self.plot_contour_regions(ax, ij, regions)
+        else:
+            self.plot_contour_distances(ax, levels)
+        self.plot_heatmap_data(ax, ij, interpolation)
+        ax.set_title(label)
         self.i += 1
 
     def get_figsize(self):
         return (7*self.columns, 7*self.rows)
 
-    def plot_contour_distances(self, ax, structure, ij, levels):
-        am, length = structure.get_alignment_map(ij, full=True)
-        fill = 1000
-        distances = np.full([length, length], fill)
-        structure_distances = structure.get_distance_matrix()
-        for i1, i2 in enumerate(am):
-            for j1, j2 in enumerate(am):
-                distances[i2, j2] = structure_distances[i1, j1]
-        if levels is None:
-            levels = {"ct": [5], "pdb": [20, 500]}[structure.datatype]
+    def plot_contour_regions(self, ax, ij, regions):
+        matrix = np.full([ij.length, ij.length], 0)
+        for (mn1, mx1), (mn2, mx2) in regions:
+            matrix[mn1-1:mx1-1, mn2-1:mx2-1] += 1
+        levels = [0.5]
         cmap = LinearSegmentedColormap.from_list('contours', ['black', 'gray'])
-        x_y = list(range(1, length+1))
+        x_y = list(range(1, ij.length+1))
+        ax.contour(x_y, x_y, matrix, levels=levels, cmap=cmap, linewidths=1)
+
+    def plot_contour_distances(self, ax, levels):
+        structure = self.structure
+        distances = structure.get_distance_matrix()
+        if levels is None:
+            levels = {"ct": [5],
+                      "ss": [5],
+                      "pdb": [20, 500]
+                      }[structure.datatype]
+        cmap = LinearSegmentedColormap.from_list('contours', ['black', 'gray'])
+        x_y = list(range(1, structure.length+1))
         ax.contour(x_y, x_y, distances, levels=levels, cmap=cmap,
                    linewidths=0.5)
 
-    def plot_heatmap_data(self, ax, structure, ij):
+    def plot_heatmap_data(self, ax, ij, interpolation):
+        structure = self.structure
         data = ij.data.copy()
         metric = ij.metric
         columns = ["i", "j", metric]
@@ -42,13 +62,13 @@ class Heatmap(Plot):
             data = data[columns+["Sign"]]
             data[metric] = data[metric]*data["Sign"]
         data = data[columns]
-        am, length = ij.get_alignment_map(structure, full=True)
+        am = ij.get_alignment_map(structure)
+        length = structure.length
         data_im = np.full([length, length], ij.fill)
         window = ij.window
         for _, i, j, value in data.itertuples():
             i = am[i-1]
             j = am[j-1]
-            data_im[i:i+window, j:j+window] = value
             data_im[j:j+window, i:i+window] = value
         min_max = ij.min_max
         if metric == "Percentile":
@@ -70,4 +90,4 @@ class Heatmap(Plot):
             cmap[0, :] = [1, 1, 1, 0]
         cmap = ListedColormap(cmap)
         ax.imshow(data_im, cmap=cmap, vmin=min_max[0], vmax=min_max[1],
-                  interpolation='none')
+                  interpolation=interpolation)
