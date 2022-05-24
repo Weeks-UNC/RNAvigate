419f41c78f02fb6a17dc10fdeab541a304067287
Patrick Irving <psirving@gmail.com>
2022-05-24 11:59:45 -0400

    updates for rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'nbsphinx',
     'sphinx_rtd_theme',
 ]
-
+nbsphinx_kernel_name = 'docs'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
