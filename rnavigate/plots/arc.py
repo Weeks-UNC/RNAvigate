ea9e5847b4e3a108c30f618427b9d39aa16d0946
Patrick Irving <psirving@gmail.com>
2022-08-22 23:40:10 -0400

    improved appearance, setting stage for annotation

--- rnavigate/plots/arc.py
+++ rnavigate/plots/arc.py
@@ -13,28 +13,36 @@ class AP(Plot):
             self.nt_length = region[1]-region[0]+1
             self.region = region
         super().__init__(num_samples, **kwargs)
-        for i in range(self.length):
-            self.set_axis(self.get_ax(i))
         self.pass_through = ["ax", "colorbar", "seqbar", "title", "ij_panel",
-                             "ij2_panel", "ct_panel"]
+                             "ij2_panel", "ct_panel", "annotations"]
 
-    def set_axis(self, ax, xticks=20, xticks_minor=5):
+    def set_axis(self, ax, annotations=0, xticks=20, xticks_minor=10):
         ax.set_aspect('equal')
         ax.yaxis.set_visible(False)
         ax.spines['left'].set_color('none')
         ax.spines['right'].set_color('none')
-        ax.spines['bottom'].set_position('zero')
+        ax.spines['bottom'].set(position=('data', -2*annotations),
+                                visible=False)
         ax.spines['top'].set_color('none')
         height = min(300, self.nt_length/2)
         mn, mx = self.region
         ax.set(xlim=(mn - 0.5, mx + 0.5),
-               ylim=(-height-5, height+1),
-               xticks=list(range(xticks, mx, xticks)))
+               ylim=(-height-1-(2*annotations), height+1),
+               xticks=list(range(xticks, mx, xticks)),
+               axisbelow=False)
+        ax.set_xticks(list(range(xticks_minor, mx, xticks_minor)), minor=True)
+        for label in ax.get_xticklabels():
+            label.set_bbox({"facecolor": "white",
+                            "edgecolor": "None",
+                            "alpha": 0.5,
+                            "boxstyle": "round,pad=0.1,rounding_size=0.2"})
 
     def plot_data(self, ct, comp, ij, ij2, profile, label, ax=None,
                   colorbar=True, seqbar=True, title=True, ij_panel="bottom",
-                  ij2_panel="bottom", ct_panel="top"):
+                  ij2_panel="bottom", ct_panel="top", annotations=[]):
         ax = self.get_ax(ax)
+        annotations = len(annotations) + seqbar
+        self.set_axis(ax=ax, annotations=annotations)
         if colorbar:
             ax_ins1 = ax.inset_axes([0.05, 0.2, 0.3, 0.03])
             self.view_colormap(ax_ins1, ij)
@@ -45,17 +53,21 @@ class AP(Plot):
                 self.view_colormap(ax_ins3, "ct_compare")
             else:
                 self.view_colormap(ax_ins3, ct)
-        self.add_patches(ax, ct, ct_panel, comp)
-        self.add_patches(ax, ij, ij_panel)
-        self.add_patches(ax, ij2, ij2_panel)
+        self.add_patches(ax=ax, data=ct, panel=ct_panel,
+                         annotations=annotations, comp=comp)
+        self.add_patches(ax=ax, data=ij, panel=ij_panel,
+                         annotations=annotations)
+        self.add_patches(ax=ax, data=ij2, panel=ij2_panel,
+                         annotations=annotations)
         self.plot_profile(ax, profile, ct)
         if seqbar:
-            self.add_sequence(ax, ct.sequence, yvalue=0.5)
+            self.add_sequence(ax, ct.sequence, yvalue=1-(2*annotations),
+                              ytrans="data")
         if title:
             self.add_title(ax, label)
         self.i += 1
 
-    def add_patches(self, ax, data, panel, comp=None):
+    def add_patches(self, ax, data, panel, annotations, comp=None):
         if comp is not None:
             ij_colors = data.get_ij_colors(comp)
         elif data is not None:
@@ -74,7 +86,7 @@ class AP(Plot):
                 theta1 = 0
                 theta2 = 180
             elif panel == "bottom":
-                center = ((i+j)/2., -2)
+                center = ((i+j)/2., -2*annotations)
                 theta1 = 180
                 theta2 = 360
             radius = 0.5+(j-i)/2.
@@ -94,12 +106,8 @@ class AP(Plot):
     def plot_profile(self, ax, profile, ct):
         if profile is None:
             return
-        column = {"RNP": "normedP",
-                  "profile": "Norm_profile",
-                  "dance": "Norm_profile"}[profile.datatype]
-        factor = {"RNP": 1,
-                  "profile": 5,
-                  "dance": 5}[profile.datatype]
+        column = profile.default_column
+        factor = profile.ap_scale_factor
         am = profile.get_alignment_map(ct)
         values = np.full(ct.length, np.nan)
         colormap = ct.get_colors("profile", profile=profile)
