ade83c2709ff23f6af91ace2e065e08876f2bee2
Patrick Irving <psirving@gmail.com>
2022-09-01 12:05:59 -0400

    added get_ss_class for high-level init

--- rnavigate/data/__init__.py
+++ rnavigate/data/__init__.py
@@ -1,5 +1,5 @@
 from .data import Data, get_nt_color, get_pairs_sens_PPV
-from .ct import CT, DotBracket, XRNA, VARNA, NSD, CTE
+from .ct import CT, DotBracket, XRNA, VARNA, NSD, CTE, get_ss_class
 from .interactions import Interactions, RINGMaP, PAIRMaP, PairProb, SHAPEJuMP
 from .log import Log
 from .pdb import PDB
@@ -7,7 +7,7 @@ from .profile import Profile, SHAPEMaP, DanceMaP, DeltaProfile, RNPMaP
 from .annotation import Annotation, Motif, ORFs
 
 __all__ = ["Data", "get_nt_color", "get_pairs_sens_PPV",
-           "CT", "DotBracket", "XRNA", "VARNA", "NSD", "CTE",
+           "CT", "DotBracket", "XRNA", "VARNA", "NSD", "CTE", "get_ss_class",
            "Interactions", "RINGMaP", "PAIRMaP", "PairProb", "SHAPEJuMP",
            "Log",
            "PDB",
