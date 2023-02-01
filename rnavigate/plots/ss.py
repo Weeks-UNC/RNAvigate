dc5202096d264c4c550ddda27fc4c115ab13afaf
Patrick Irving <psirving@gmail.com>
2023-02-01 09:47:04 -0500

    aesthetic customization for ss plots

--- rnavigate/plots/ss.py
+++ rnavigate/plots/ss.py
@@ -8,6 +8,31 @@ from matplotlib.collections import LineCollection
 
 class SS(Plot):
     def __init__(self, num_samples, structure, **kwargs):
+        self.plot_params = {
+            "structure_lw": 3,
+
+            "data_lw": 1.5,
+            "data_a": None,
+
+            "annotations_lw": 30,
+            "annotations_s": 30**2,
+            "annotations_a": 0.4,
+
+            "marker_s": 10**2,
+
+            "structure_z": 0,
+            "basepair_z": 0,
+            "backbone_z": 0,
+            "annotations_z": 5,
+            "nucleotide_z": 10,
+            "data_z": 15,
+            "sequence_z": 20,
+            "position_z": 25,
+        }
+        kw_list = list(kwargs.keys())
+        for kw in kw_list:
+            if kw in self.plot_params:
+                self.plot_params[kw] = kwargs.pop(kw)
         self.structure = structure
         xmin = min(self.structure.xcoordinates)
         xmax = max(self.structure.xcoordinates)
@@ -24,13 +49,6 @@ class SS(Plot):
                    ylim=[ymin-3*ybuffer, ymax+ybuffer])
         self.pass_through = ["colors", "sequence", "apply_color_to",
                              "colorbar", "title", "positions", "bp_style"]
-        self.zorder = {"structure": 0,
-                       "backbone": 0,
-                       "annotations": 5,
-                       "nucleotide": 10,
-                       "data": 15,
-                       "sequence": 20,
-                       "position": 25}
 
     def plot_data(self, interactions, interactions2, profile, annotations,
                   label,
@@ -84,10 +102,29 @@ class SS(Plot):
         points = np.array([x, y]).T.reshape(-1, 1, 2)
         segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
-        zorder = self.zorder["structure"]
-        lc = LineCollection(segments, colors=colors, zorder=zorder)
+        current_color = None
+        new_segments = []
+        new_colors = []
+        for segment, color in zip(segments, colors):
+            if current_color is None:
+                current_color = color
+                current_segment = list(segment)
+            elif current_color == color:
+                current_segment.append(segment[-1])
+            else:
+                new_segments.append(current_segment)
+                new_colors.append(current_color)
+                current_color = color
+                current_segment = list(segment)
+        new_segments.append(current_segment)
+        new_colors.append(current_color)
+
+        zorder = self.plot_params["structure_z"]
+        lc = LineCollection(new_segments, colors=new_colors, zorder=zorder,
+                            lw=self.plot_params["structure_lw"])
         ax.add_collection(lc)
 
+        zorder = self.plot_params["basepair_z"]
         for pair in ss.pairList():
             x = ss.xcoordinates[[p-1 for p in pair]]
             y = ss.ycoordinates[[p-1 for p in pair]]
@@ -129,8 +166,8 @@ class SS(Plot):
     def plot_sequence(self, ax, profile, colors, sequence, apply_color_to,
                       positions, bp_style):
         ss = self.structure
-        nuc_z = self.zorder["nucleotide"]
-        seq_z = self.zorder["sequence"]
+        nuc_z = self.plot_params["nucleotide_z"]
+        seq_z = self.plot_params["sequence_z"]
         valid_apply = ["background", "sequence", "structure", None]
         message = f"invalid apply_color_to, must be in {valid_apply}"
         assert apply_color_to in valid_apply, message
@@ -142,7 +179,8 @@ class SS(Plot):
                                          ct=self.structure)
             self.plot_structure(ax, struct_color, bp_style=bp_style)
             ax.scatter(ss.xcoordinates, ss.ycoordinates, marker=".",
-                       c=struct_color, zorder=seq_z)
+                       c=struct_color, zorder=seq_z,
+                       s=self.plot_params["marker_s"])
             return
         if apply_color_to == "background":
             bg_color = ss.get_colors(colors, profile=profile,
@@ -175,24 +213,21 @@ class SS(Plot):
                 ax.scatter(xcoords, ycoords, marker=marker, s=100,
                            c=nt_color[mask], lw=1, zorder=seq_z)
 
-    def add_lines(self, ax, i, j, color, linewidth=1.5):
+    def add_lines(self, ax, i, j, color):
         ss = self.structure
-        zorder = self.zorder["data"]
+        zorder = self.plot_params["data_z"]
+        alpha = self.plot_params["data_a"]
         x = [ss.xcoordinates[i-1], ss.xcoordinates[j-1]]
         y = [ss.ycoordinates[i-1], ss.ycoordinates[j-1]]
-        ax.plot(x, y, color=color, linewidth=linewidth, zorder=zorder)
+        ax.plot(x, y, color=color, lw=self.plot_params[
+            "data_lw"], zorder=zorder, alpha=alpha)
 
     def plot_interactions(self, ax, interactions, colorbar, cmap_pos):
         if interactions is None:
             return
         ij_colors = interactions.get_ij_colors()
-        window = interactions.window
-        if window == 1:
-            lw = 3
-        else:
-            lw = 6
         for i, j, color in zip(*ij_colors):
-            self.add_lines(ax, i, j, color, linewidth=lw)
+            self.add_lines(ax, i, j, color)
         if colorbar:
             x, width = [(0, 0.49), (0.51, 0.49)][cmap_pos]
             ax_ins1 = ax.inset_axes([x, 0, width, 0.05])
@@ -200,7 +235,7 @@ class SS(Plot):
 
     def plot_positions(self, ax, text_color, bbox_color, spacing=20):
         ss = self.structure
-        zorder = self.zorder["position"]
+        zorder = self.plot_params["position_z"]
         for i in range(spacing, ss.length, spacing):
             ax.annotate(f"{ss.sequence[i-1]}\n{i}",
                         xy=(ss.xcoordinates[i-1], ss.ycoordinates[i-1]),
@@ -213,22 +248,28 @@ class SS(Plot):
 
     def plot_annotation(self, ax, annotation):
         color = annotation.color
-        zorder = self.zorder["annotations"]
+        alpha = self.plot_params["annotations_a"]
+        size = self.plot_params["annotations_s"]
+        linewidth = self.plot_params["annotations_lw"]
+        zorder = self.plot_params["annotations_z"]
         if annotation.annotation_type == "spans":
             for start, end in annotation.spans:
                 x = self.structure.xcoordinates[start-1:end]
                 y = self.structure.ycoordinates[start-1:end]
-                ax.plot(x, y, color=color, alpha=0.2, lw=30, zorder=zorder)
+                ax.plot(x, y, color=color, alpha=alpha, lw=linewidth,
+                        zorder=zorder)
         elif annotation.annotation_type == "sites":
-            x = self.structure.xcoordinates[annotation.sites]
-            y = self.structure.ycoordinates[annotation.sites]
-            ax.scatter(x, y, color=color, marker='o', ec="none", alpha=0.7,
-                       s=30**2, zorder=zorder)
+            sites = np.array(annotation.sites)-1
+            x = self.structure.xcoordinates[sites]
+            y = self.structure.ycoordinates[sites]
+            ax.scatter(x, y, color=color, marker='o', ec="none", alpha=alpha,
+                       s=size, zorder=zorder)
         elif annotation.annotation_type == "groups":
             for group in annotation.groups:
                 x = self.structure.xcoordinates[group["sites"]]
                 y = self.structure.ycoordinates[group["sites"]]
                 color = group["color"]
-                ax.plot(x, y, color=color, alpha=0.2, lw=30, zorder=zorder)
-                ax.scatter(x, y, color=color, marker='o', ec="none", alpha=0.4,
-                           s=30**2, zorder=zorder)
+                ax.plot(x, y, color=color, a=alpha,
+                        lw=linewidth, zorder=zorder)
+                ax.scatter(x, y, color=color, marker='o', ec="none", a=alpha,
+                           s=size, zorder=zorder)
