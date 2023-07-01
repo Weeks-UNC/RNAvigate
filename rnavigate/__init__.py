2bfabe9c36a00f3ef6e7fa8646b512d7ef55671b
Patrick Irving <psirving@gmail.com>
2023-07-01 00:53:38 -0400

    Simplified imports

--- rnavigate/__init__.py
+++ rnavigate/__init__.py
@@ -1,19 +1,1 @@
 from .rnavigate import *
-from . import data, plots, analysis, styles
-
-sample = ["Sample",
-          "create_code_button",
-          "get_color_list"]
-
-arrays = ["array_qc",
-          "array_ap",
-          "array_skyline",
-          "array_ss",
-          "array_heatmap",
-          "array_mol",
-          "array_circle",
-          "array_linreg"]
-
-modules = ["plots", "data", "analysis", "styles"]
-
-__all__ = sample + arrays + modules
--- rnavigate/plots/__init__.py
+++ rnavigate/plots/__init__.py
@@ -1,16 +1,13 @@
+from .plots import Plot
 from .arc import AP
 from .circle import Circle
 from .disthist import DistHist
 from .heatmap import Heatmap
 from .linreg import LinReg
 from .mol import Mol
-from .plots import Plot
 from .qc import QC
 from .skyline import Skyline, Profile
 from .sm import SM
 from .ss import SS
 from .roc import ROC
 from .alignment import Alignment
-
-__all__ = ["AP", "Circle", "DistHist", "Heatmap", "LinReg", "Mol", "Plot",
-           "QC", "Skyline", "SM", "SS", "ROC", "Profile", "Alignment"]
