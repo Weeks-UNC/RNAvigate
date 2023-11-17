b9f77dffedb420b9b40861605da4b82178d64149
Patrick Irving <psirving@gmail.com>
2023-11-17 13:59:01 -0500

    (move) transcriptomics in own module

--- rnavigate/transcriptomics/__init__.py
+++ rnavigate/transcriptomics/__init__.py
@@ -1,3 +1,15 @@
-from rnavigate.transcriptomics.bed import *
-from rnavigate.transcriptomics.transcriptome import *
-from rnavigate.transcriptomics.eclip import *
+from rnavigate.transcriptomics.bed import BedFile, NarrowPeak
+from rnavigate.transcriptomics.transcriptome import Transcript, Transcriptome
+from rnavigate.transcriptomics.eclip import (
+    eCLIPDatabase,
+    download_eclip_peaks,
+    )
+
+__all__ = [
+    "BedFile",
+    "NarrowPeak",
+    "Transcriptome",
+    "Transcript",
+    "eCLIPDatabase",
+    "download_eclip_peaks",
+    ]
