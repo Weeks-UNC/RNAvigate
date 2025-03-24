85dbfd521c8e2a228933de6664f4ffc0daaee4df
scottallen877 <113545368+scottallen877@users.noreply.github.com>
2025-03-24 15:32:17 -0400

    fix: another bug in plot_auroc

--- rnavigate/analysis/auroc.py
+++ rnavigate/analysis/auroc.py
@@ -190,7 +190,7 @@ class WindowedAUROC:
         ax.set_xticks(ticks=[x for x in range(100, stop, 100) if x > start], minor=True)
         ax.tick_params(axis="x", which="major", labelsize=36)
         plots.adjust_spines(ax, ["bottom"])
-        ax.grid(ax="x")
+        ax.grid(axis="x")
 
         # set figure size so that 100 ax units == 1 inch
         plot.set_figure_size(height_ax_rel=1 / 100, width_ax_rel=1 / 100)
